86eb69e3aa4ef7a80b1ed04a5327ed47bd4b285f
simla04 <jessicasowi@gmail.com>
2022-12-07 14:51:40 +0100

    fine

---
+++ assets/main.js
@@ -0,0 +1,50 @@
+var app = new Vue ({
+    el: '#app',
+    data: {
+        // *rendo l'index dinamico così
+        
+        objImmagini: [
+            {
+                url: './assets/img/01.jpg',
+                alt: 'descrizione immagine 1'
+            },
+            {
+                url: './assets/img/02.jpg',
+                alt: 'descrizione immagine 2'
+            },
+            {
+                url: './assets/img/03.jpg',
+                alt: 'descrizione immagine 3'
+            },
+            {
+                url: './assets/img/04.jpg',
+                alt: 'descrizione immagine 4'
+            },
+            {
+                url: './assets/img/05.jpg',
+                alt: 'descrizione immagine 5'
+            },
+        ],
+        indexImmagini: 0,
+    },
+    methods: {
+        immagineSuccessiva() {
+            // cambio indice in avanti
+            // controllo che l'indice sia minore della lunghezza dell'oggetto
+            if( this.indexImmagini < this.objImmagini.length-1 ) {
+            this.indexImmagini++;
+            } else {
+                this.indexImmagini = 0;
+            }
+        },
+        immaginePrecedente() {
+            // se l'indice è minore di zero, torna all'ultimo elemento
+            if( this.indexImmagini <= 0) {
+                this.indexImmagini = this.objImmagini-1;
+            } else {
+                this.indexImmagini--;
+            }
+        }
+
+    }
+})
